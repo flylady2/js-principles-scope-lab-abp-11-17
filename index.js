2cb4ecfb0b6f3660e45841f810c00075535d7c82
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-15 03:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
+leastFavoriteCustomer = 'Wolfgang';
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase();
 }
@@ -8,4 +9,7 @@ function setBestCustomer() {
 }
 function overwriteBestCustomer() {
   bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Harry';
 }
